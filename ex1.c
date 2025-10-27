a4cc4e19a76153900136ff2cb0f20c28329d5d69
MohammedAbdulcelil <mohammed536460@gmail.com>
2025-10-27 22:20:20 +0300

    Add number guessing game in ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+
+int main() {
+    int guess;
+    int secret_number = 3;
+
+    printf("Guess the number between 1 and 5: ");
+    scanf("%d", &guess);
+
+    if (guess == secret_number) {
+        printf("Congratulations! You guessed the right number.\n");
+    } else {
+        printf("Oops! That's incorrect. The secret number was %d.\n", secret_number);
+    }
+
+    return 0;
+}
